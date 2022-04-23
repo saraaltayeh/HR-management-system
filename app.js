7f83639a07610d2927f4350ca946fff3d50e66d5
Sara Al-tayeh <saraaltayeh1@gmail.com>
2022-04-24 02:44:38 +0300

    changes day 8

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ Employee.prototype.render = function(){
     let imageCard = document.createElement("img");
     imageCard.src = this.imageUrl;
     sectionEl.appendChild(imageCard);
-    
+
     let nameCard = document.createElement('p');
     nameCard.textContent = this.fullName;
     sectionEl.appendChild(nameCard);
@@ -46,7 +46,6 @@ Employee.prototype.render = function(){
     salaryCard.textContent = this.salary;
     sectionEl.appendChild(salaryCard);
 
-// document.write(`employee name: ${this.fullName}, ID: ${this.employeeId}, salary: ${this.salary}<br>`);
 }
     
 Employee.prototype.salary = function() {
