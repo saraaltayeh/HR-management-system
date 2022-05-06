fc9b13b8f6a27e9127c4708f973652e176e3af49
Sara Al-tayeh <saraaltayeh1@gmail.com>
2022-05-06 16:30:20 +0300

    changes

--- accounting.js
+++ accounting.js
@@ -59,15 +59,6 @@ Employee.prototype.salary = function() {
         return avg;
         }
 
-// let ghazi = new Employee("Administration", 1, 400, 300);
-// let lana = new Employee("Marketing", 2, 400,350);
-// let tamara = new Employee("Administration", 3, 300,450 );
-// let safi = new Employee("Mid-Senior", 4, 300,350 );
-// let omar = new Employee( "Development", 5, 300, 500);
-// let rana = new Employee("Development", 6, 200,550);
-// let hadi = new Employee("Finance", 7, 500,600);
-
-
 function randomSalary(min ,max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -77,7 +68,6 @@ function renderAll() {
         allEmployee[i].renderTable();
     }
 }
-
 
 function handleSubmit(event) {
     event.preventDefault();
